23168477ba23a850f182b1f3f74ac8e19a9d6a1b
Sheesh <vladsolokov@gmail.com>
2023-01-01 19:46:48 +0600

    liching

--- Assets/Scripts/Network/RpgNetworkManager.cs
+++ Assets/Scripts/Network/RpgNetworkManager.cs
@@ -7,10 +7,9 @@ namespace Aquapunk
 {
     public class RpgNetworkManager : NetworkManager
     {
-        public Transform startPos;
         public override void OnServerAddPlayer(NetworkConnectionToClient conn)
         {
-            GameObject player = Instantiate(playerPrefab, startPos.position, startPos.rotation);
+            GameObject player = Instantiate(playerPrefab);
             //player.GetComponent<Player>().canvas = FindObjectOfType<Canvas>();
             NetworkServer.AddPlayerForConnection(conn, player);
         }
