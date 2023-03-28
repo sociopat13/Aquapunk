3dd497628204ddf141e17d1fe874eed180fb18b2
Sheesh <vladsolokov@gmail.com>
2023-03-28 18:54:42 +0600

    bug fix

--- Assets/Scripts/Enviroment/GenerateLandscape.cs
+++ Assets/Scripts/Enviroment/GenerateLandscape.cs
@@ -1,17 +1,47 @@
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class GenerateLandscape : NetworkBehaviour
 {
     public GameObject planePrefab;
 
-    
-    private void Start()
+
+    [SerializeField]
+    private int _rols;
+    [SerializeField]
+    private int _cols;
+
+
+    public override void OnStartServer()
     {
-        if (isServer)
+        base.OnStartServer();
+
+        Generate();
+    }
+
+    private void Generate()
+    {
+        List<GameObject> list = new List<GameObject>();
+        float factorX = planePrefab.transform.localScale.x * 10;
+        float factorZ = planePrefab.transform.localScale.z * 10;
+        for (float obj = 0, x = 0 - factorX * _rols / 2; obj <= _rols; obj++, x += factorX)
         {
+            for (float objZ = 0, z = 0 - factorZ * _cols / 2; objZ <= _cols; objZ++, z += factorZ)
+            {
+                Transform plane = Instantiate(planePrefab).transform;
+                
+                plane.position = new Vector3(x, 0, z);
+
+                plane.SetParent(transform);
+
+                list.Add(plane.gameObject);
+
+                NetworkServer.Spawn(plane.gameObject);
+            }
         }
+        transform.Rotate(new Vector3(0, 45, 0));
     }
 }
